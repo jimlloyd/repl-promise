9feeb878baf3f6433d7e32a7ed6a70d2924d9278
Jim Lloyd <jim.lloyd@gmail.com>
2014-09-04 20:28:29 -0700

    Several improvements and unit test.
    This version now does its own processing of lines, in a way that
    guarantees that each line will be process by the full chain
    of promises before the next line is read. The output is therefore
    deterministic even when input is piped from a file.
    
    One unit test is provided, which demonstrates that the input
    script can return a promise, in which case the script interpreter
    waits until the promise is fulfilled in order to print the result.

---
+++ test/Transcript-test.js
@@ -0,0 +1,36 @@
+// # Transcript-test.js
+
+'use strict';
+
+require('./support/config');
+
+var repl = require('../lib/index.js');
+var fs = require('fs');
+var concat = require('concat-stream');
+
+function transcriptTest(name, done) {
+    var expected = fs.readFileSync('test/data/'+name+'.expected', { encoding: 'utf8' });
+    var expectedLines = expected.split('\n');
+
+    var output = concat({encoding: 'string'}, function(data) {
+        var dataLines = data.split('\n');
+        expect(dataLines).to.deep.equal(expectedLines);
+        done();
+    });
+
+    var options = {
+        prompt: 'node > ',
+        input: fs.createReadStream('test/data/'+name+'.txt'),
+        output: output,
+    };
+
+    repl.start(options);
+}
+
+describe('Transcript', function() {
+
+    it('should produce the expected transcript given delay-promise.txt', function(done) {
+        this.timeout(5000);
+        transcriptTest('delay-promise', done);
+    });
+});
